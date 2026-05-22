b27a4068f7d96cf172a5d79596d7e7d24b52acec
Sam15b <mannansyed660@gmail.com>
2026-05-22 21:14:26 +0530

    Middleware Push

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
-const isPublicRoute = createRouteMatcher(["/api/webhook/clerk(.*)", "/api/graph/(.*)"])
+const isPublicRoute = createRouteMatcher(["/api/webhook/clerk(.*)", "/api/graph(.*)"])
 const isAuthRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
 
 export default clerkMiddleware(async (auth, request) => {
